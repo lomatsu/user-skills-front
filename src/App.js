0a8d6cb62e77e27bb593b0235c4ca67d53a320f4
Lauro Omatsu <lauroomatsu@gmail.com>
2022-07-19 00:52:32 -0300

    add signin

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Signin from './Signin';
+import Profile from './Profile';
 
 function App() {
+  const token = localStorage.getItem('token');
+
+  if(!token) {
+    return <Signin />
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+          <Route path="/">
+            <Profile />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
